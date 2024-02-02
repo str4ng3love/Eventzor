3080437198279820751f87e7ec76bd90843efc4c
str4ng3love <normalemailadress@gmail.com>
2024-02-02 12:14:45 +0100

    added Image component as a bg instead of loading it via css

--- app/events/layout.tsx
+++ app/events/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import EventCarousel from "../components/dynamic/Events/EventCarousel";
 import { getNewestEvents } from "@/helpers/getEvent";
 import Footer from "../components/static/Footer";
+import Image from "next/image";
 
 
 export const metadata: Metadata = {
@@ -19,12 +20,22 @@ export default async function layout({
     <>
       <main className="flex flex-col items-center min-h-screen">
 
-        <div className="bg-event-hero relative lg:before:hidden before:absolute before:top-0 before:bg-gradient-radial before:to-transparent before:via-transparent before:h-full before:w-full before:from-slate-900 w-full bg-cover bg-center bg-no-repeat transition-all">
+        <div className="relative lg:before:hidden before:absolute before:top-0 before:bg-gradient-radial before:to-transparent before:via-transparent before:h-full before:w-full before:from-slate-900 w-full bg-cover bg-center bg-no-repeat transition-all">
           <EventCarousel
             heading="Newest Events"
             items={events}
             darkBgAlpha={false}
             fullWidthBlur={true}
+          />
+          <Image
+            alt="bacground image"
+            src={'/images/hero_event.jpeg'}
+            fill
+            placeholder="blur"
+            blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+            style={{ objectFit: "cover" }}
+            sizes="(max-width: 750px"
+            className={`-z-10`}
           />
         </div>
 
--- app/market/layout.tsx
+++ app/market/layout.tsx
@@ -2,37 +2,48 @@ import { Metadata } from "next";
 import { getNewestItems } from "@/helpers/getIItem";
 import ItemCarousel from "../components/dynamic/Market/ItemCarousel";
 import Footer from "../components/static/Footer";
+import Image from "next/image";
 
 
 export const metadata: Metadata = {
-    title: "Eventzor | Market",
-    description: "Dashboard Demo",
+  title: "Eventzor | Market",
+  description: "Dashboard Demo",
 };
 export default async function layout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    const {items} = await getNewestItems()
+  const { items } = await getNewestItems()
 
-    return (
-      <>        <main className="flex flex-col items-center min-h-screen ">
+  return (
+    <>        <main className="flex flex-col items-center min-h-screen ">
 
-        <div className="relative bg-item-hero w-full bg-cover bg-center bg-no-repeat transition-all lg:before:hidden before:absolute before:top-0 before:bg-gradient-radial before:to-transparent before:via-transparent before:h-full before:w-full  before:from-slate-900">
-          <ItemCarousel
-            heading="Newest Items"
-            items={items}
-            darkBgAlpha={false}
-            fullWidthBlur={true}
-          />
-        </div>
+      <div className="relative w-full bg-cover bg-center bg-no-repeat transition-all lg:before:hidden before:absolute before:top-0 before:bg-gradient-radial before:to-transparent before:via-transparent before:h-full before:w-full  before:from-slate-900">
+        <ItemCarousel
+          heading="Newest Items"
+          items={items}
+          darkBgAlpha={false}
+          fullWidthBlur={true}
+        />
+        <Image
+          alt="bacground image"
+          src={'/images/hero_item.jpeg'}
+          fill
+          placeholder="blur"
+          blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+          style={{ objectFit: "cover" }}
+          sizes="(max-width: 750px"
+          className={`-z-10`}
+        />
+      </div>
 
-       
-        {children}
-       
-      </main>
+
+      {children}
+
+    </main>
       <Footer />
-      </>
+    </>
 
-    );
+  );
 }
